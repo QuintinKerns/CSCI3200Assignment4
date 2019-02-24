9a617616d6462c15b78d067f8efaf1dbc1a72600
QuintinKerns <qakern8560@ung.edu>
2019-02-24 17:47:57 -0500

    Number 6 done

--- src/Number6.java
+++ src/Number6.java
@@ -1,9 +1,32 @@
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Number6 {
+	static Scanner scan = new Scanner(System.in);
+	static TreeMap map;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		map = new TreeMap();
+		askForName();
+		while (map.size() > 0){
+			Entry e = map.firstEntry();
+			System.out.println("Key: " + e.getKey() + ", Value: " + e.getValue());
+			map.remove(e.getKey(), e.getValue());
+		}
+	}
+	
+	private static void askForName(){
+		System.out.println("Please enter a name: ");
+		String name = scan.nextLine();
+		int vowels = 0;
+		for (char c : name.toCharArray()){
+			if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') vowels++;
+		}
+		map.put(name, vowels);
+		System.out.println("Press enter to continue, type no to quit:");
+		String response = scan.nextLine();
+		if (response.equals("")) askForName();
 	}
 
 }
